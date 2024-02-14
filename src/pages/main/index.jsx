135a8f8bdd84fcdb49b58769b8650b7479780e30
yousef_walid <yw012276@gmail.com>
2024-02-14 22:42:26 +0200

    solving conflict

--- src/pages/main/index.jsx
+++ src/pages/main/index.jsx
@@ -1,9 +1,13 @@
 import  Signboard from "./component/Signboard";
 import Cards from "./component/Cards";
 import DonateForm from './component/Donate';
+import Navbar  from "../../commn/Navabar";
+import Navabar2 from "../../commn/navbar";
 
 const Main = ()=>(
 <>
+<Navbar/>
+<Navabar2/>
   <Signboard/>
   <Cards/>
   <DonateForm/>
