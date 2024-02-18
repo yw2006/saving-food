dd06d15f2879f2995d99d09bcfe6d250d610ae1f
yousef_walid <yw012276@gmail.com>
2024-02-18 22:20:16 +0200

    handle main route

--- src/App.jsx
+++ src/App.jsx
@@ -9,17 +9,18 @@ import Donate from "./pages/main/component/Donate";
 import Main from "./pages/main/index";
 import SignUp from "./pages/Sign up/Signup";
 import Login from "./pages/Login/Login";
+import FoodDonationForm from "./pages/donationForm/componets/form";
+import Faq from "./pages/Faq/component/Faq"
+import Error from './pages/404/component/Error'
 const App = () => {
   return (
     <Routes>
-      <Route
-        path="/"
-        element={
-          <Main/>
-        }
-      />
-      <Route path="/signup" element={<SignUp/>}/>
-      <Route path="/login" element={<Login/>}/>
+      <Route path="/" element={<Main />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/donation" element={<FoodDonationForm />} />
+      <Route path="/fqa" element={<Faq />} />
+      <Route path="*" element={<Error />} />
     </Routes>
   );
 };
